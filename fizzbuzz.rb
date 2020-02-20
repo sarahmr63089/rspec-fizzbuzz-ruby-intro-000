83c6d36748ea87d28d5e46c8c0d76b69c5030815
Sarah Rose <sarahrose63089@gmail.com>
2020-02-20 20:24:30 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,11 +2,11 @@
 # See README.md for instructions on how to do this
 def fizzbuzz(int)
     if int % 3 == 0
-      puts "fizz"
+      puts "Fizz"
     elsif int % 5 == 0
-      puts "buzz"
+      puts "Buzz"
     elsif (int % 3 == 0) && (int % 5 == 0)
-      puts "fizzbuzz"
+      puts "FizzBuzz"
     else
     end
 end
