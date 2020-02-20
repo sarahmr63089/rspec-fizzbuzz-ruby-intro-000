add98bd7c47fa36aa56d195f4ca26c05898725f1
Sarah Rose <sarahrose63089@gmail.com>
2020-02-20 20:19:11 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,12 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+def fizzbuzz(int)
+    if int % 3 && int !% 5
+      puts "fizz"
+    elsif int % 5 && int !% 3
+      puts "buzz"
+    elsif int % 3 && int % 5
+      puts "fizzbuzz"
+    else
+    end
+end
