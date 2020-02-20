b08ec7bcfd17f18f0ed3d6ede48afb9c6dc745d0
Sarah Rose <sarahrose63089@gmail.com>
2020-02-20 20:27:09 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,12 +1,12 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(int)
-    if int % 3 == 0
+    if (int % 3 == 0) && (int % 5 != 0)
       return "Fizz"
-    elsif int % 5 == 0
-      puts "Buzz"
+    elsif (int % 5 == 0) && (int % 3 != 0)
+      return "Buzz"
     elsif (int % 3 == 0) && (int % 5 == 0)
-      puts "FizzBuzz"
+      return "FizzBuzz"
     else
     end
 end
